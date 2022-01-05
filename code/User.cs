2541873b3a2853f3388708f17a3c54cef4817bbb
rndtrash <6745157+rndtrash@users.noreply.github.com>
2022-01-05 22:03:50 +0300

    MP3 playback from the internet

--- code/User.cs
+++ code/User.cs
@@ -1,9 +1,15 @@
-﻿using Sandbox;
+﻿using MP3Sharp;
+using Sandbox;
+using Sandbox.Internal;
+using System;
 
 namespace SandOS
 {
 	partial class User : Player
 	{
+		protected Sound music;
+		protected SoundStream musicSS;
+
 		public override void Respawn()
 		{
 			Camera = new FixedCamera();
@@ -15,11 +21,57 @@ namespace SandOS
 		public override void Simulate( Client cl )
 		{
 			base.Simulate( cl );
+
+			if ( IsClient )
+				DebugOverlay.ScreenText( $"{musicSS.QueuedSampleCount}" );
 		}
 
 		public override void OnKilled()
 		{
 			return; // NOOP
+		}
+
+		[ClientCmd( "sandos_loadurl" )]
+		public static void CmdPlayURL( string url )
+		{
+			(Local.Pawn as User)?.LoadMusicFromWeb( url );
+		}
+
+		public async void LoadMusicFromWeb( string url )
+		{
+			music.Stop();
+
+			var result = await new Http( new Uri( url ) ).GetStreamAsync();
+			var mp3Stream = new MP3Stream( result );
+
+			music = Sound.FromScreen( "audiostream.default" );
+			musicSS = music.CreateStream( sampleRate: mp3Stream.Frequency, channels: mp3Stream.ChannelCount );
+
+			var buffer = new byte[8192];
+			var delay = 1000 * buffer.Length / sizeof( short ) / mp3Stream.ChannelCount / mp3Stream.Frequency;
+			Log.Info( $"{delay}" );
+
+			int bytesRead = -1, bytesTotal = 0;
+			while ( !music.Finished && bytesRead != 0 )
+			{
+				if ( musicSS.QueuedSampleCount < buffer.Length * 2 )
+				{
+					bytesRead = mp3Stream.Read( buffer, 0, buffer.Length );
+					var data = new short[bytesRead / 2];
+					for ( int i = 0; i < data.Length; i++ )
+					{
+						data[i] = (short)(buffer[i * 2] | (buffer[i * 2 + 1] << 8));
+					}
+
+					musicSS.WriteData( new Span<short>( data ) );
+					bytesTotal += bytesRead;
+				} else
+					await GameTask.DelayRealtime( Math.Max( delay, 1 ) );
+			}
+
+			Log.Info( $"read {bytesTotal} bytes" );
+
+			mp3Stream.Close();
 		}
 	}
 }
